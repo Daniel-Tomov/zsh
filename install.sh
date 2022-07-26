33ea04fd67eaf4c88a4613e89e11521679937dae
Daniel Tomov <90072819+Daniel-Tomov@users.noreply.github.com>
2022-07-26 00:00:42 -0400

    initial

---
+++ install.sh
@@ -0,0 +1,14 @@
+
+#install zsh and any other stuff
+sudo apt install curl git zsh -y
+curl -L git.io/antigen > antigen.zsh
+
+#install colorized cat
+sudo apt install golang-go -y
+go get -u github.com/owenthereal/ccat
+sudo cp /home/$USER/go/bin/ccat /usr/bin/ccat
+sudo cp /home/$USER/go/bin/ccat /usr/bin/cat
+
+# copy files to home directory
+cp .zshrc ~
+cp .p10k.zsh ~
